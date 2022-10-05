4c3bcb5738694349d00dfcf7d9a57a6977606bb6
Seb Scothorn <seb.scothorn@dropless.co.uk>
2022-10-06 00:41:43 +0100

    Updated API property descriptions

--- src/weather-reports/dto/weather-reports-by-postcode.dto.ts
+++ src/weather-reports/dto/weather-reports-by-postcode.dto.ts
@@ -1,13 +1,13 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsDateString, IsOptional } from 'class-validator';
 export class WeatherReportsByPostcodeDto {
-	@ApiProperty({ required: false, description: 'Weather reports from and including this date-time', format: 'date-time' })
 	@IsOptional()
+	@ApiProperty({ required: false, description: 'Weather reports from this date-time (inclusive)', format: 'date-time' })
 	@IsDateString()
 	from?: string;
 
-	@ApiProperty({ required: false, description: 'Weather reports up to this date-time', format: 'date-time' })
 	@IsOptional()
+	@ApiProperty({ required: false, description: 'Weather reports up to this date-time (exclusive)', format: 'date-time' })
 	@IsDateString()
 	to?: string;
 }
