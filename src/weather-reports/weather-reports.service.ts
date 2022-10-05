bd6226280b63ad28d3e98dba181a0d7382d359cd
Seb Scothorn <seb.scothorn@dropless.co.uk>
2022-10-06 00:42:42 +0100

    Now filters returned weatherReports by from and to date times

--- src/weather-reports/weather-reports.service.ts
+++ src/weather-reports/weather-reports.service.ts
@@ -5,11 +5,11 @@ import { WeatherReport } from './entities/weather-report.entity';
 export class WeatherReportsService {
 	// Temp local db until connected to actual db
 	private weatherReports: WeatherReport[] = [
-		{ id: 1, postcode: 'BS12AB', dateTime: new Date() },
-		{ id: 2, postcode: 'BS12AB', dateTime: new Date() },
-		{ id: 3, postcode: 'BS13CD', dateTime: new Date() },
-		{ id: 4, postcode: 'BS14EF', dateTime: new Date() },
-		{ id: 5, postcode: 'BS12AB', dateTime: new Date() },
+		{ id: 1, postcode: 'BS12AB', dateTime: new Date('2022-10-06T09:00:00Z') },
+		{ id: 2, postcode: 'BS12AB', dateTime: new Date('2022-10-07T09:00:00Z') },
+		{ id: 3, postcode: 'BS13CD', dateTime: new Date('2022-10-06T09:00:00Z') },
+		{ id: 4, postcode: 'BS14EF', dateTime: new Date('2022-10-06T09:00:00Z') },
+		{ id: 5, postcode: 'BS12AB', dateTime: new Date('2022-10-06T11:00:00Z') },
 	];
 
 	findAll(): WeatherReport[] {
@@ -17,6 +17,8 @@ export class WeatherReportsService {
 	}
 
 	findByPostcode(postcode: string, from?: Date, to?: Date): WeatherReport[] {
-		return this.weatherReports.filter((weatherReport) => weatherReport.postcode === postcode);
+		return this.weatherReports.filter((weatherReport) => {
+			return weatherReport.postcode === postcode && (from == null || weatherReport.dateTime >= from) && (to == null || weatherReport.dateTime < to);
+		});
 	}
 }
