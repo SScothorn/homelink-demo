e9343bd787f582bf68933894fa2850ae0e5d96d5
Seb Scothorn <seb.scothorn@dropless.co.uk>
2022-10-05 22:11:11 +0100

    Added swagger docs

--- src/weather-reports/entities/weather-report.entity.ts
+++ src/weather-reports/entities/weather-report.entity.ts
@@ -1,4 +1,9 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class WeatherReport {
+	@ApiProperty()
 	id: number;
+
+	@ApiProperty()
 	postcode: string;
 }
--- src/weather-reports/weather-reports.controller.ts
+++ src/weather-reports/weather-reports.controller.ts
@@ -1,16 +1,20 @@
 import { Controller, Get, Param } from '@nestjs/common';
+import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { WeatherReport } from './entities/weather-report.entity';
 import { WeatherReportsService } from './weather-reports.service';
 
+@ApiTags('Weather Reports')
 @Controller('weather-reports')
 export class WeatherReportsController {
 	constructor(private weatherReportsService: WeatherReportsService) {}
 
+	@ApiOkResponse({ type: Array<WeatherReport>, isArray: true })
 	@Get()
 	getWeatherReports(): WeatherReport[] {
 		return this.weatherReportsService.findAll();
 	}
 
+	@ApiOkResponse({ type: WeatherReport, isArray: true })
 	@Get(':postcode')
 	getWeatherReportsByPostCode(@Param('postcode') postcode: string): WeatherReport[] {
 		return this.weatherReportsService.findByPostcode(postcode);
